1221b38b98dcdd49468c7c8fe9098598ea0eb8fc
inter <167876607+moussainter@users.noreply.github.com>
2025-07-05 04:49:52 +0200

    La fin de ma facture

--- facture.py
+++ facture.py
@@ -20,7 +20,7 @@ html = """
       table {
         border-collapse: collapse;
         width: 70%;
-        margin: 20px;
+        margin-left: 20px;
       }
       th,
       td {
@@ -44,11 +44,40 @@ html = """
       #btn {
         background-color: #4caf50;
         color: white;
-        padding: 10px 20px;
+        padding: center;
         border: none;
-        cursor: pointer;
-        margin-left: 20px;
+        margin-top: 15px;
+        style: bold;
+        width: 100px;
+        height: 35px;
+        
+        
       }
+      
+      #tableau, #button {
+        border: solid 1px #333;
+        float: left;
+        }
+        
+      #button {
+        margin-left: 20px;
+        width: 150px;
+        height: 35px;
+        }
+        
+      #plus, #moins {
+        width: 55px;
+        height: 35px;
+        background-color: gray;
+        }
+      #submit  {
+        clear: both;
+        margin-top: 20px;
+        border-top: solid 1px #333;
+       
+      }
+        
+        
     </style>
   </head>
   <body>
@@ -69,61 +98,120 @@ html = """
           <option value="clt4">Client 4</option>
         </select>
       </div>
-      <div class="form">
-        <table>
-          <thead>
+      
+      <div>
+     
+      <div class="form" id="tableau">
+        <table border="1" cellspacing="0" cellpadding="5" id="table">
+         
             <tr>
               <th>Produit</th>
               <th>Quantité</th>
               <th>Prix Unitaire (€)</th>
               <th>Total (€)</th>
             </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td><input type="text" name="produit" id="produit1" /></td>
-              <td><input type="text" name="quantite" class="element" id="quantite1" /></td>
-              <td><input type="text" name="prix" class="element" id="prix1" /></td>
-              <td><input type="text" name="total" class="sousTotal" id="total1" readonly /></td>
-            </tr>
-            <tr>
-              <td><input type="text" name="produit" id="produit2" /></td>
-              <td><input type="text" name="quantite" class="element" id="quantite2" /></td>
-              <td><input type="text" name="prix" class="element" id="prix2" /></td>
-              <td><input type="text" name="total" class="sousTotal" id="total2" readonly /></td>
-            </tr>
-            <tr>
-              <td><input type="text" name="produit" id="produit3" /></td>
-              <td><input type="text" name="quantite" class="element" id="quantite3" /></td>
-              <td><input type="text" name="prix" class="element" id="prix3" /></td>
-              <td><input type="text" name="total" class="sousTotal" id="total3" readonly /></td>
-            </tr>
-
-            <tr>
+         
+         
+           
+            <tr id="totalHT">
               <td colspan="3" class="center">Total HT</td>
-              <td><input type="text" name="totalHT" id="totalHT">&nbsp;€</td>
-            </tr>
+              
+              <td><input type="text" name="totalHT" id="totalHT1">&nbsp;€</td>
+            </tr> 
             <tr>
               <td colspan="3" class="center">TVA (20%)</td>
+              
               <td><input type="text" name="tva" id="tva">&nbsp;€</td>
             </tr>
-          </tbody>
-          <tfoot>
+          
+        
             <tr>
               <td colspan="3" class="center">Total TTC</td>
+              
               <td><input type="text" name="montantTTC" id="montantTTC">&nbsp;€</td>
             </tr>
-          </tfoot>
+         
         </table>
-
+        </div>
+        <div id="button">
+          <input type="button" id="plus" value="+" />
+          <input type="button" id="moins" value="-" />
+        </div>
+         </div>
+        
+      <div class="center" id="submit">
+        
         <input type="submit" value="Valider" id="btn" />
         <br /><br />
       </div>
+      
     </form>
-  </body>
+ 
   <script>
-  
+           
+            
+    function crationLigne(name, id, tr, ligneTotalHT, type, typezone) {
+      
+      let td = document.createElement("td");
+      let input = document.createElement("input");
+      input.setAttribute("type", typezone );
+      input.setAttribute("name", name);
+      input.setAttribute("id", id );
+      
+      if(type!= undefined) {
+        input.setAttribute("class", type);
+        
+        if (type == "element") input.addEventListener("change", produit, false)
+        }
+      
+      td.appendChild(input);
+      tr.appendChild(td);
+      
+      let parent = ligneTotalHT.parentNode;
+      parent.insertBefore(tr, ligneTotalHT);
+     
+      
+    }
+
+    function ajoutLigne() {
+      let lignes = document.getElementsByClassName("ligne");
+      let numeroLigne = lignes.length + 1;
+      let ligneTotalHT = document.getElementById("totalHT");
+      let tr = document.createElement("tr");
+      tr.setAttribute("class", "ligne");
+      tr.setAttribute("id", "ligne" + numeroLigne);
+      crationLigne("produit", "produit"+ numeroLigne, tr, ligneTotalHT, "text");
+      crationLigne("quantite", "quantite"+numeroLigne, tr, ligneTotalHT, "element", "text");
+      crationLigne("prix", "prix"+numeroLigne, tr, ligneTotalHT, "element", "text");
+      crationLigne("total", "total"+numeroLigne, tr, ligneTotalHT, "sousTotal", "text");
+      crationLigne("sup", "sup"+numeroLigne, tr, ligneTotalHT, "sup", "checkbox");
+     
+    }
+    
+    function supprimerLigne() {
+      let sups = document.getElementsByClassName("sup");
+      let j = 0;
+      let lignes = Array();
+      for (let i = 0; i < sups.length; i++) {
+        if(sups[i].checked){
+          let id = sups[i].getAttribute("id");
+          let position = id.substring(id.length - 1, id.length);
+          lignes[j] = "ligne" + position;
+          j++;
+        }
+      }
+      
+      for (let i = 0; i < lignes.length; i++) {
+        let ligne = document.getElementById(lignes[i]);
+        let parent = ligne.parentNode;
+        parent.removeChild(ligne);
+      }
+      calculTotal() 
+    
+    }
+    
     function produit() {
+      
         let  num = this.getAttribute("id");
       
         let position = num.substring(num.length - 1, num.length);
@@ -142,14 +230,17 @@ html = """
         
         else {
         total.value = sousTotal;
+        calculTotal()
         }
+      }
         
-        let montantTotal = 0
+    function calculTotal() {
+       let montantTotal = 0
         let sousTotals = document.getElementsByClassName("sousTotal");
         for (let i = 0; i < sousTotals.length; i++) {
           if (!isNaN(parseFloat(sousTotals[i].value))) montantTotal = montantTotal + parseFloat(sousTotals[i].value);   
         }
-        let totalHT = document.getElementById("totalHT");
+        let totalHT = document.getElementById("totalHT1");
          totalHT.value = montantTotal;
          
         let tva = document.getElementById("tva");
@@ -157,14 +248,22 @@ html = """
         
         let montantTTC = document.getElementById("montantTTC");
           montantTTC.value = montantTotal + parseFloat(tva.value);
-        
-        
     }
+        
+        
            
     let elements = document.getElementsByClassName("element");
     for (let i = 0; i < elements.length; i++) {
         elements[i].addEventListener("change", produit, false);
     }
+    
+    let plus = document.getElementById("plus");
+    plus.addEventListener("click", ajoutLigne, false);
+    
+    let moins = document.getElementById("moins");
+    moins.addEventListener("click", supprimerLigne, false);
+    
+    
     
   </script>
 </html>
